c9c0c6c04a89f0813388a04b9e6ab47e4d941546
OlegLegezo <oleglegezo@yandex.ru>
2022-05-15 10:45:50 +0300

    задача 2 решение

---
+++ ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.Write("введите номер четверти от 1 до 4:");
+int number = int.Parse(Console.ReadLine());
+
+
+if (number == 1)
+{
+    Console.WriteLine("x>0 и y>0");
+}
+
+else if (number == 2)
+{
+    Console.WriteLine("x<0 и y>0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("x<0 и y<0");
+}
+
+else if (number == 4)
+{
+    Console.WriteLine("x>0 и y<0");
+}
+else
+{
+    Console.WriteLine("значение вне диапазона");
+}
