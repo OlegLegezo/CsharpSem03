ea70ee1dbf3702665c143a0e7fe600d0bc315222
OlegLegezo <oleglegezo@yandex.ru>
2022-05-15 10:16:28 +0300

    задача 1 решение

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 17-ая задача про определение четвертей на координатной оси
+
+Console.Write("введите значение х: ");
+int x=int.Parse(Console.ReadLine ());
+Console.Write("введите значение y: ");
+int y=int.Parse(Console.ReadLine ());
+
+if (x>0 && y>0)
+{
+    Console.WriteLine("1-ая четверть");
+}
+
+else if (x<0 && y>0)
+{
+    Console.WriteLine("2-ая четверть");
+}
+else if (x<0 && y<0)
+{
+    Console.WriteLine("3-ая четверть");
+}
+
+else if (x>0 && y<0)
+{
+    Console.WriteLine("4-ая четверть");
+}
